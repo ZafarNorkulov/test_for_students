e445d52b347d8f8e8fc4730b89726fb3dd178eee
ZafarNorkulov <norqulovzafar123@gmail.com>
2024-05-28 17:10:14 +0500

    header initial design created

--- src/layouts/userLayout.tsx
+++ src/layouts/userLayout.tsx
@@ -1,6 +1,50 @@
 import { ReactElement } from "react";
-
+import Logo from "../assets/logo.jpg";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../store/auth";
+import { Modal } from "antd";
+import { useAppDispatch } from "../store";
 const UserLayout = ({ children }: { children: ReactElement }) => {
-  return <div>UserLayout{children}</div>;
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { confirm } = Modal;
+  const showDeleteConfirm = () => {
+    confirm({
+      title: "Are you sure logout?",
+      // icon: <ExclamationCircleFilled />,
+      okText: "Yes",
+      okType: "danger",
+      cancelText: "No",
+      onOk() {
+        dispatch(logout());
+        navigate("/signin");
+      },
+      onCancel() {
+        console.log("Cancel");
+      },
+    });
+  };
+  return (
+    <div>
+      <header className="w-screen flex items-center justify-between">
+        <Link
+          className="logo flex items-center max-w-[30%] text-[#111] text-[1.1rem] uppercase gap-x-[5px]"
+          to={"/"}
+        >
+          <img
+            src={Logo}
+            alt="Logo"
+            className="w-[22%] rounded-full"
+            loading="lazy"
+          />
+          Buxoro Xalqaro Tibbiyot Texnikumi
+        </Link>
+        <button className="bg-blue-500 text-white" onClick={showDeleteConfirm}>
+          Logout
+        </button>
+      </header>
+      {children}
+    </div>
+  );
 };
 export default UserLayout;
