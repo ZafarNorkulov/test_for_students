e7f2b13ab21780bb03343cf9934104e9b0fe211c
ZafarNorkulov <norqulovzafar123@gmail.com>
2024-06-28 09:15:00 +0500

    change

--- src/store/auth/index.ts
+++ src/store/auth/index.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { QueryClient } from "@tanstack/react-query";
 import SignIn from "./service";
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 export type TypeInitialStateAuth = {
   access_token?: string | null;
@@ -72,6 +72,6 @@ const SignInSlice = createSlice({
   },
 });
 
-export const { login, logout, loadingAuth } = SignInSlice.actions;
+// export const { login, logout, loadingAuth } = SignInSlice.actions;
 
 export default SignInSlice.reducer;
