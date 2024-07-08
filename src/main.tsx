613c70829713037c05ebf123edc39049cb3477b2
ZafarNorkulov <norqulovzafar123@gmail.com>
2024-07-08 15:06:11 +0500

    Analitics vercel component add

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import store from "./store/index.ts";
+import { Analytics } from '@vercel/analytics/react';
 
 const queryClient = new QueryClient();
 
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <App />
+        <Analytics />
       </QueryClientProvider>
     </Provider>
   </Router>
